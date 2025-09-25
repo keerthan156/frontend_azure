388178041d65a2ac621a83e3afc1e9abccebb98e
keerthan <keerthan>
2025-09-25 18:15:42 +0530

    testing

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+// Backend URL for the application
+const BACKEND_URL = 'my-backend.internal.calmfield-7fe6f435.westus2.azurecontainerapps.io:8080';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
